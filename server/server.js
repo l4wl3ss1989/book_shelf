a83e9f9738a06471a5edb1f5acfa271c5b8c6f20
unknown <l4wl3ss1989@gmail.com>
2019-02-26 23:01:55 +0100

    user request added Sever Basics done

--- server/server.js
+++ server/server.js
@@ -12,6 +12,7 @@ mongoose.connect(config.DATEBASE);
 // Setting Models
 const { User } = require('./models/user');
 const { Book } = require('./models/book');
+const { auth } = require('./middleware/auth');
 
 // Setting Middlewares
 app.use(bodyParse.json());
@@ -19,6 +20,23 @@ app.use(cookieParser());
 
 /*** Setting Requests ***/
 // GET //
+app.get('/api/auth',auth,(req, res)=>{
+    res.json({
+        isAuth: true,
+        id: req.user._id,
+        email: req.user.email,
+        name: req.user.name,
+        lastname: req.user.lastname
+    })
+})
+
+app.get('/api/logout', auth, (req, res) => {
+    req.user.deleteToken(req.token, (err, user) => {
+        if (err) return res.status(400).send(err);
+        res.sendStatus(200);
+    })
+})
+
 app.get('/api/getBook', (req, res) => {
     let id = req.query.id;
 
@@ -39,6 +57,32 @@ app.get('/api/books', (req, res) => {
     })
 })
 
+app.get('/api/getReviewer', (req, res) => {
+    let id = req.query.id;
+
+    User.findById(id, (err, doc) => {
+        if (err) return res.status(400).send(err);
+        res.json({
+            name: doc.name,
+            lastname: doc.lastname
+        })
+    })
+})
+
+app.get('/api/users', (req, res) => {
+    User.find({}, (err, users) => {
+        if (err) return res.status(400).send(err);
+        res.status(200).send(users)
+    })
+})
+
+app.get('/api/user_posts', (req, res) => {
+    Book.find({ ownerId: req.query.user }).exec((err, docs) => {
+        if (err) return res.status(400).send(err);
+        res.send(docs);
+    })
+})
+
 // POST //
 app.post('/api/book', (req, res) => {
     const book = new Book(req.body)
@@ -52,9 +96,42 @@ app.post('/api/book', (req, res) => {
     })
 })
 
+app.post('/api/register', (req, res) => {
+    const user = new User(req.body);
+
+    user.save((err, doc) => {
+        if (err) return res.json({ success: false })
+        res.status(200).json({
+            success: true,
+            user: doc
+        })
+    })
+})
+
+app.post('/api/login', (req, res) => {
+    User.findOne({ 'email': req.body.email }, (err, user) => {
+        if (!user) return res.json({ isAuth: false, message: 'Auth failed email not found' })
+
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            if (!isMatch) return res.json({
+                isAuth: false,
+                message: 'Wrong password'
+            })
+            user.generateToken((err, user) => {
+                if (err) return res.status(400).send(err);
+                res.cookie('auth', user.token).json({
+                    isAuth: true,
+                    id: user._id,
+                    email: user.email
+                })
+            })
+        })
+    })
+})
+
 // UPDATE //
 app.post('/api/book_update', (req, res) => {
-    Book.findByIdAndUpdate(req.body._id,req.body,{new:true},(err,doc)=>{
+    Book.findByIdAndUpdate(req.body._id, req.body, { new: true }, (err, doc) => {
         if (err) return res.status(400).send(err);
         res.json({
             success: true,
@@ -64,10 +141,10 @@ app.post('/api/book_update', (req, res) => {
 })
 
 // DELETE //
-app.delete('/api/delete_book',(req,res)=>{
+app.delete('/api/delete_book', (req, res) => {
     const id = req.query.id;
 
-    Book.findByIdAndRemove(id,(err,doc)=>{
+    Book.findByIdAndRemove(id, (err, doc) => {
         if (err) return res.status(400).send(err);
         res.json(true)
     })
